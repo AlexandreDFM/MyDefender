7b55de15797a8c897bfd32c628e708a0a7228408
Alexandre <alexandre-kevin.de-freitas-martins@epitehc.eu>
2022-03-20 23:51:25 +0100

    Fix Fonction too long

--- src/pause_2.c
+++ src/pause_2.c
@@ -7,34 +7,34 @@
 
 #include "my_defender.h"
 
-void config_sound_2(sfRenderWindow *win, defender_t *defender, game_t *game, int i)
+void config_sound_2(sfRenderWindow *win, defender_t *def, game_t *game, int i)
 {
-    defender->v_save = defender->v_music == 0 ?
-    defender->v_save : defender->v_music;
-    defender->v_buf_save = defender->volume_buf == 0 ?
-    defender->v_buf_save : defender->volume_buf;
-    defender->v_buf2_save = defender->volume_buf2 == 0 ?
-    defender->v_buf2_save: defender->volume_buf2;
+    def->v_save = def->v_music == 0 ?
+    def->v_save : def->v_music;
+    def->v_buf_save = def->volume_buf == 0 ?
+    def->v_buf_save : def->volume_buf;
+    def->v_buf2_save = def->volume_buf2 == 0 ?
+    def->v_buf2_save: def->volume_buf2;
 }
 
-void config_sound(sfRenderWindow *win, defender_t *defender, game_t *game, int i)
+void config_sound(sfRenderWindow *win, defender_t *def, game_t *game, int i)
 {
-    config_sound_2(win, defender, game, i);
+    config_sound_2(win, def, game, i);
     if (i == 5) {
-        sfMusic_setVolume(defender->g_music, defender->v_music
-        == 0 ? defender->v_save : 0);
+        sfMusic_setVolume(def->g_music, def->v_music
+        == 0 ? def->v_save : 0);
     } else if (i == 6) {
-        sfSound_setVolume(defender->slash, defender->volume_buf
-        == 0 ? defender->v_buf_save: 0);
-        sfSound_setVolume(defender->towertk, defender->volume_buf
-        == 0 ? defender->v_buf_save: 0);
-        sfSound_setVolume(defender->towerpl, defender->volume_buf
-        == 0 ? defender->v_buf_save: 0);
-        sfSound_setVolume(defender->towerdl, defender->volume_buf
-        == 0 ? defender->v_buf_save: 0);
+        sfSound_setVolume(def->slash, def->volume_buf
+        == 0 ? def->v_buf_save: 0);
+        sfSound_setVolume(def->towertk, def->volume_buf
+        == 0 ? def->v_buf_save: 0);
+        sfSound_setVolume(def->towerpl, def->volume_buf
+        == 0 ? def->v_buf_save: 0);
+        sfSound_setVolume(def->towerdl, def->volume_buf
+        == 0 ? def->v_buf_save: 0);
         for (int i = 0; i < 4; i++) {
-            sfSound_setVolume(defender->pop[i], defender->volume_buf2 ==
-            0 ? defender->v_buf2_save: 0);
+            sfSound_setVolume(def->pop[i], def->volume_buf2 ==
+            0 ? def->v_buf2_save: 0);
         }
     }
 }
